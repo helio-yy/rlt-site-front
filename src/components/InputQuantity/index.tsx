bce2acd9760db379148570651b2c5dfc4c08a912
helio-yy <helio@yaayoo.com.br>
2022-04-04 12:41:50 -0300

    Ajustes component quantity

--- src/components/InputQuantity/index.tsx
+++ src/components/InputQuantity/index.tsx
@@ -3,48 +3,17 @@ import EmailOutlinedIcon from '@material-ui/icons/EmailOutlined'
 import Add from '@material-ui/icons/Add'
 import Remove from '@material-ui/icons/Remove'
 
-import { InputHTMLAttributes, useCallback, useState } from 'react'
 import * as S from './styles'
 
-interface IInputQuantityProps extends InputHTMLAttributes<HTMLInputElement> {
-  name: string
-  sobtitle?: string
-}
-
-const InputQuantity: React.FC<IInputQuantityProps> = ({
-  name,
-  sobtitle,
-  defaultValue
-}) => {
-  const [inputRefByTransferr, setInputRefByTransferr] =
-    useState<React.MutableRefObject<HTMLInputElement>>()
-
-  const handleIncrement = useCallback(() => {
-    if (inputRefByTransferr) {
-      inputRefByTransferr.current.value = String(
-        Number(inputRefByTransferr.current.value) + 1
-      )
-    }
-  }, [inputRefByTransferr])
-
-  const handleDecrement = useCallback(() => {
-    if (inputRefByTransferr && Number(inputRefByTransferr.current.value) > 0) {
-      inputRefByTransferr.current.value = String(
-        Number(inputRefByTransferr.current.value) - 1
-      )
-    }
-  }, [inputRefByTransferr])
-
+const InputQuantity = () => {
   return (
     <S.Wrapper>
-      {sobtitle && <p className="sobtitle">{sobtitle}</p>}
-
       <S.InputWrapper>
-        <span className="material-icons" onClick={handleDecrement}>
+        <span className="material-icons">
           <Remove />
         </span>
-        <TextField name={name} type="number" />
-        <span className="material-icons" onClick={handleIncrement}>
+        <TextField type="number" />
+        <span className="material-icons">
           <Add />
         </span>
       </S.InputWrapper>
